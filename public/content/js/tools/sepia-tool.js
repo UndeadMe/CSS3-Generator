02c39366284638bcaa4a1571babf1819f12e114e
Banana021s <arash.k021@yahoo.com>
2021-10-15 22:56:32 +0330

    create clipboard for sepia

--- public/content/js/tools/sepia-tool.js
+++ public/content/js/tools/sepia-tool.js
@@ -4,6 +4,10 @@ const validationSepiaElem = document.querySelector(".sepia-validation-elem")
 const previewImage = document.querySelector(".img-left")
 const cssCodeBtn = document.querySelector(".css-code-btn")
 const generateWrapBox = document.querySelector(".generate-wrap-box")
+const codePennel = document.querySelector(".code-pannel")
+const styleInp = document.querySelector("#styleInp")
+const copyToClipboard = document.querySelector(".clipboard-btn")
+const cssCodeCloseBtn = document.querySelector(".close-generate-pannel-btn")
 
 let Sepia = {
     elemClass: null,
@@ -67,15 +71,31 @@ const checkValidateInputs = (value) => {
     
 }
 
+let StyleText = undefined
 //? open generate pannel
 const openGeneratePannel = () => {
     if (Sepia.isInit) {
         if (Sepia.sepiaValidate) {
             generateWrapBox.classList.add("active")
+            StyleText = `.${Sepia.elemClass} { filter: sepia(${Sepia.sepia}); }`
+            codePennel.innerHTML = `.${Sepia.elemClass} { <br>
+                &nbsp;&nbsp;&nbsp; filter: sepia(${Sepia.sepia}); <br>
+            }`
         } else 
             alert("please enter the information correctly")
     } else
         alert("please complete the fields above")
+}
+
+//? copy to clipboard
+const copyToClipboardText = () => {
+    styleInp.value = StyleText
+    navigator.clipboard.writeText(styleInp.value);
+}
+
+//? close generator pannel
+const closeGeneratePannel = () => {
+    generateWrapBox.classList.remove("active")
 }
 
 //? add style to image
@@ -87,3 +107,5 @@ addStyleToImage()
 elemClassNameInp.addEventListener("keyup", checkInit)
 cssCodeBtn.addEventListener("click", openGeneratePannel)
 sepiaInp.addEventListener("keyup", (e) => checkValidateInputs(e.target.value))
+cssCodeCloseBtn.addEventListener("click", closeGeneratePannel)
+copyToClipboard.addEventListener("click", copyToClipboardText)
