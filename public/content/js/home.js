e66289fab99d9ddfea699abdeb4f0519021750d4
Banana021s <arash.k021@yahoo.com>
2021-10-18 22:27:28 +0330

    create search feature for home page
    it helps to users find high speed their tools

---
+++ public/content/js/home.js
@@ -0,0 +1,50 @@
+const toolBoxButtons = document.querySelectorAll(".tool-box-button")
+const searchBtn = document.querySelector(".search-btn")
+const searchInp = document.querySelector(".search-input")
+const scrollToTopCircle = document.querySelector(".scrollToTop-circle")
+
+//? find tool box based by user search
+const findToolBoxes = () => {
+    //? get search value
+    const searchValue = searchInp.value
+    if (searchValue.length !== 0) {
+        //? delete all class active for tool box buttons
+        deleteActiveClass(toolBoxButtons)
+
+        //? find search tool box
+        let SearchToolBox = Array.from(toolBoxButtons).find(button => {
+            return String(button.dataset.tool).toLowerCase().includes(searchValue.toLowerCase())
+        })
+        
+        //? scroll to this tool
+        window.scrollTo({
+            top: SearchToolBox.getBoundingClientRect().top - 50,
+            behavior: "smooth"
+        })
+
+        SearchToolBox.classList.add("active")
+    }
+}
+
+//? delete class active 
+const deleteActiveClass = (elements) => {
+    elements.forEach(item => {
+        item.classList.remove("active")
+    })
+}
+
+//? go to top scroll , scroll top = 0
+const goToTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+}
+
+searchBtn.addEventListener("click", findToolBoxes)
+scrollToTopCircle.addEventListener("click", goToTop)
+searchInp.addEventListener("keyup", (e) => {
+    if (e.target.value.length === 0) 
+        deleteActiveClass(toolBoxButtons)
+    e.key === "Enter" ? findToolBoxes() : ""
+})
