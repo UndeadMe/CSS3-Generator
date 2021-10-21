e05cba7d859c94f8ec4eb5e5d03870654df75394
Banana021s <arash.k021@yahoo.com>
2021-10-21 22:41:24 +0330

    fix comming soon links in home page and etc
    and fix links in comming soon page

--- comming-soon.php
+++ comming-soon.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Comming Soon</title>
+    <link rel="icon" href="public/content/img/css-img/css.png" sizes="any">
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap-reboot.min.css">
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.css">
